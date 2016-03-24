e7aa429dfe8523e6e5c487768112ccbef0c72f0f
curtjenk <curtjenk@comcast.net>
2016-03-23 21:25:09 -0400

    Search by type = movie, tv & person.
    Need to fix modal and add search type = ALL

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@ var baseURL = 'https://api.themoviedb.org/3/';
 var apiKey = 'api_key=2d9e9aacbdc397236dad8339b3d2c17c';
 var totalPagesNowPlaying = 0;
 var methodNowPlaying = 'movie/now_playing';
-var methodSearch = 'search/movie?query=';
+var methodSearchAll = 'search/------?query=';
+var methodSearchMovie = 'search/movie?query=';
+var methodSearchTV = 'search/tv?query=';
+var methodSearchPerson = 'search/person?query=';
 var methodPopular = 'movie/popular';
 var methodTopRated = 'movie/top_rated';
 var methodUpcoming = 'movie/upcoming';
@@ -16,6 +19,7 @@ $(document).ready(function() {
     getListOfMovies();
     // $('input[type="submit"').click(search);
     $('#movie-search-form').submit(function() {
+
         search();
         event.preventDefault(); //don't let the form submit
     });
@@ -25,18 +29,37 @@ $(document).ready(function() {
 });
 
 function search() {
-    var movieSearchTerm = $('input[type="text').val();
-    var searchURL = baseURL + methodSearch + movieSearchTerm + '&' + apiKey;
+    var searchTerm = $('input[type="text').val();
+    if (searchTerm.length < 1 || searchTerm === null || searchTerm === "") return;
+
+    var searchURL = '';
+    var searchBy = $('#search-by').val();
+    switch (searchBy) {
+        case 'all':
+        case 'tv':
+            searchURL = baseURL + methodSearchTV + searchTerm + '&' + apiKey;
+            break;
+        case 'movie':
+            searchURL = baseURL + methodSearchMovie + searchTerm + '&' + apiKey;
+            break;
+        case 'person':
+            searchURL = baseURL + methodSearchPerson + searchTerm + '&' + apiKey;
+            break;
+        default:
+            searchURL = baseURL + methodSearchMovie + searchTerm + '&' + apiKey;
+    }
+    searchURL = encodeURI(searchURL);
+    // console.log(searchURL);
     $.getJSON(searchURL, populatePosterGrid);
 }
 
 function getNowPlaying() {
     var nowPlayingURL = baseURL + methodNowPlaying + '?' + apiKey;
-    console.log(nowPlayingURL);
+    // console.log(nowPlayingURL);
     $.getJSON(nowPlayingURL, function(movieData) {
         totalPagesNowPlaying = movieData.total_pages;
-        console.log(movieData);
-        console.log('Total Pages Now Playing = ' + totalPagesNowPlaying);
+        //console.log(movieData);
+        //console.log('Total Pages Now Playing = ' + totalPagesNowPlaying);
         populatePosterGrid(movieData);
 
         //get page 2
@@ -68,6 +91,7 @@ function getImagesBaseURL() {
     });
 }
 // used to populate the type ahead for searching
+// only called at startup!!!
 function getListOfMovies() {
 
     var popularURL = baseURL + methodPopular + '?' + apiKey;
@@ -82,71 +106,112 @@ function getListOfMovies() {
 
             $.getJSON(upcomingURL, function(list) {
                 populateTypeAheadSource(list);
-
-                // console.log(typeAheadSource);
-                // console.log(typeAheadSource.length);
             });
 
         });
     });
 }
 
-function populatePosterGrid(movieData) {
+function populatePosterGrid(data) {
 
-    var resultsLength = movieData.results.length;
+    var newHtml = '';
+    var searchBy = $('#search-by').val();
+    switch (searchBy) {
+        case 'all':
+        case 'tv':
+        case 'movie':
+            newHtml = popGridMovieData(data);
+            break;
+        case 'person':
+            // different format;
+            console.log('----------- Person Search results ----------');
+            console.log(data);
+            newHtml = popGridPersonData(data);
+            break;
+        default:
+            newHtml = popGridMovieData(data);
+    }
+
+    $('#poster-grid').html(newHtml);
+}
+
+function popGridMovieData(data) {
+    var resultsLength = data.results.length;
     var movieHtml = "";
     for (i = 0; i < resultsLength; i++) {
-        var title = movieData.results[i].title;
-        var posterPath = movieData.results[i].poster_path;
-        var uniqueId = movieData.results[i].id;
+        var title = data.results[i].title;
+        var posterPath = data.results[i].poster_path;
+        var uniqueId = data.results[i].id;
+        var mediaType = data.results[i].media_type;
 
         var imgSrc = imageBaseURL + 'w300' + posterPath;
 
         if (posterPath) {
             movieHtml += '<div class="col-md-2 col-sm-2">';
-            movieHtml += '<img ' + imgClassAttr + ' id="' + uniqueId + '"  src="' + imgSrc + '">';
+            movieHtml += '<img ' + imgClassAttr + ' id="' + uniqueId + '" ' + mediaType + ' src="' + imgSrc + '">';
             movieHtml += '</div>';
         }
-        // else 
-        // {
-        //     console.log(title + " : " + posterPath);
-        // }
-        //begin populating the typeAheadSource
+        //Continue "learning" -- populating the typeAheadSource
         if ($.inArray(title, typeAheadSource) === -1) typeAheadSource.push(title);
     }
-    $('#poster-grid').html(movieHtml);
+    return movieHtml;
+}
+//todo: use mediaType to get the correct details to fill the modal
+//todo: show cast in the modal (??maybe)
+function popGridPersonData(data) {
+    var resultsLength = data.results.length;
+    var newHtml = "";
+    for (i = 0; i < resultsLength; i++) {
+        for (y = 0; y < data.results[i].known_for.length; y++) {
+            var title = data.results[i].known_for[y].title;
+            var posterPath = data.results[i].known_for[y].poster_path;
+            var uniqueId = data.results[i].known_for[y].id;
+            var mediaType = data.results[i].known_for[y].media_type;
+
+            var imgSrc = imageBaseURL + 'w300' + posterPath;
+
+            if (posterPath) {
+                newHtml += '<div class="col-md-2 col-sm-2">';
+                newHtml += '<img ' + imgClassAttr + ' id="' + uniqueId + '" ' + mediaType + ' src="' + imgSrc + '">';
+                newHtml += '</div>';
+            }
+            //Continue "learning" -- populating the typeAheadSource
+            if ($.inArray(title, typeAheadSource) === -1) typeAheadSource.push(title);
+        }
+    }
+    return newHtml;
 }
 
 //modal stuff
-    $('#poster-grid').on('click', 'div > img', function() {
- // alert('do modal');
-        var currImageId = $(this).attr('id');
-        var infoURL = baseURL + 'movie/' + currImageId + '?' + apiKey;
-        var reviewURL = baseURL + 'movie/' + currImageId + '/reviews?' + apiKey;
+$('#poster-grid').on('click', 'div > img', function() {
+    // alert('do modal');
+    var currImageId = $(this).attr('id');
+    var infoURL = baseURL + 'movie/' + currImageId + '?' + apiKey;
+    var reviewURL = baseURL + 'movie/' + currImageId + '/reviews?' + apiKey;
 
-        //Get the info on the selected/clicked movie
-        $.getJSON(infoURL, function(movieData) {
-            console.log(movieData);
-            $('.modal-title').html(movieData.title);
-            var releaseDate = movieData.release_date;
-            var overview = movieData.overview;
-            var movieHtml = "";
-            movieHtml = '<h4>Released</h4>';
-            movieHtml += '<p class="indent">' + releaseDate + '</p>';
-            if (overview) {
-                movieHtml += '<h4>Overview</h4>';
-                movieHtml += '<p class="indent">' + overview + '</p>';
-            }
-            $('.modal-body').html(movieHtml);
-        });
-        //Now retrieve movie reviews
-        $.getJSON(reviewURL, function(movieData) {
-            if (movieData.results.length > 0) {
-                $('.modal-body').append('<h4>Reviews</h4>');
-            }
-            for (var i = 0; i < movieData.results.length; i++) {
-                $('.modal-body').append('<p style="font: italic bold 12px/30px Georgia, serif;">' + movieData.results[i].author + '</p>');
-                $('.modal-body').append('<p  class="indent">' + movieData.results[i].content + '</p>');
-            }
-        });
+    //Get the info on the selected/clicked movie
+    $.getJSON(infoURL, function(movieData) {
+        // console.log(movieData);
+        $('.modal-title').html(movieData.title);
+        var releaseDate = movieData.release_date;
+        var overview = movieData.overview;
+        var movieHtml = "";
+        movieHtml = '<h4>Released</h4>';
+        movieHtml += '<p class="indent">' + releaseDate + '</p>';
+        if (overview) {
+            movieHtml += '<h4>Overview</h4>';
+            movieHtml += '<p class="indent">' + overview + '</p>';
+        }
+        $('.modal-body').html(movieHtml);
     });
+    //Now retrieve movie reviews
+    $.getJSON(reviewURL, function(movieData) {
+        if (movieData.results.length > 0) {
+            $('.modal-body').append('<h4>Reviews</h4>');
+        }
+        for (var i = 0; i < movieData.results.length; i++) {
+            $('.modal-body').append('<p style="font: italic bold 12px/30px Georgia, serif;">' + movieData.results[i].author + '</p>');
+            $('.modal-body').append('<p  class="indent">' + movieData.results[i].content + '</p>');
+        }
+    });
+});
